21461425273148e7275bb503dcadf350a841b800
Abrianna <abrianna@mit.edu>
2025-02-26 22:24:00 -0500

    Basics done

--- script.js
+++ script.js
@@ -32,3 +32,26 @@ nextBtn.addEventListener("click", () => {
   currentImageIndex = (currentImageIndex + 1) % images.length;
   lightboxImg.src = images[currentImageIndex].src;
 });
+
+// Navbar Scroll Effect
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// Dynamically assign classes based on image aspect ratio
+// images.forEach((img) => {
+//   img.onload = () => {
+//     const aspectRatio = img.naturalWidth / img.naturalHeight;
+//     if (aspectRatio < 0.5) {
+//       img.classList.add("tall"); // Tall images
+//     } else if (aspectRatio > 1.5) {
+//       img.classList.add("wide"); // Wide images
+//     }
+//   };
+// });
